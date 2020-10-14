6f549da8850e5bc593d9b1382fc5262f91e337bf
Chris Pyles <cpyles@berkeley.edu>
2020-10-13 19:43:02 -0700

    fixed test

--- lab/lab07/tests/q3_4.py
+++ lab/lab07/tests/q3_4.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q3_4',
     'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> assert c == 3;\n>>> assert 5 <= q_1_star <= 15;\n>>> assert 50 <= p_1_star <= 100\n', 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> assert c == 3;\n>>> assert 5 <= q_1_star <= 15;\n>>> assert 20 <= p_1_star <= 50\n', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
